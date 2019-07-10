e5cd3a2495d8ab4b9e2b58dedf9b32d7d6e1c513
Moosees <linus.almgren@gmail.com>
2019-07-10 16:21:21 +0200

    Add header component to all routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 import Shop from './pages/shop/shop.component';
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Homepage} />
-    <Route path="/shop" component={Shop} />
-  </Switch>
+  <>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/shop" component={Shop} />
+    </Switch>
+  </>
 );
 
 export default App;
