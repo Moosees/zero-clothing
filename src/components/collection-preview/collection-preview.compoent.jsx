5f0e45c0182fb2893c490a5651a1687dbe13c9ed
Moosees <linus.almgren@gmail.com>
2019-07-10 16:04:19 +0200

    Add collection-items to collection-preview

--- src/components/collection-preview/collection-preview.compoent.jsx
+++ src/components/collection-preview/collection-preview.compoent.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => {
   const itemList = items
     .filter((_item, i) => i < 4)
-    .map(item => <div key={item.id}>{item.name}</div>);
+    .map(item => <CollectionItem key={item.id} {...item} />);
   return (
     <div className="collection-preview">
       <h1 className="title">{title}</h1>
