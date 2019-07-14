6cf2e95e826e99ce70d012272d4924c16e939280
Moosees <linus.almgren@gmail.com>
2019-07-14 17:43:02 +0200

    Add ability to sign up with email and password

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-sign-up.styles.scss';
 
-const SignInSignUp = () => <SignIn className="sign-in-sign-up" />;
-
+const SignInSignUp = () => (
+  <div className="sign-in-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 export default SignInSignUp;
