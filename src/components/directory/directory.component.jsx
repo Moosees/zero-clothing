4d63f627ad4d7ac42a27619c5ebe389574129039
Moosees <linus.almgren@gmail.com>
2019-07-25 22:30:24 +0200

    Add directory reducer and use it for sections state

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import MenuItem from '../../components/menu-item/menu-item.component';
-import { sections } from './directory.data';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import './directory.styles.scss';
 
-class Directory extends Component {
-  state = { sections };
+const Directory = ({ sections }) => {
+  const items = sections.map(({ id, ...sectionProps }) => (
+    <MenuItem key={id} {...sectionProps} />
+  ));
+  return <div className="directory-menu">{items}</div>;
+};
 
-  render() {
-    const items = this.state.sections.map(({ id, ...sectionProps }) => (
-      <MenuItem key={id} {...sectionProps} />
-    ));
-    return <div className="directory-menu">{items}</div>;
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 import userReducer from './user/user.reducer';
 
 const persistConfig = {
@@ -12,6 +13,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   cart: cartReducer,
+  directory: directoryReducer,
   user: userReducer
 });
 
