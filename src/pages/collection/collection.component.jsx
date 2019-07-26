f868d416b125960238914d7791a0ec8219889f08
Moosees <linus.almgren@gmail.com>
2019-07-27 00:15:51 +0200

    Style collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 import { selectCollection } from '../../redux/shop/shop.selectors';
-//import CollectionItem from '../../components/collection-item/collection-item.component';
 import './collection.styles.scss';
 
-const Collection = ({ collection }) => (
-  <div className="collection">
-    <h2>{collection.title}</h2>
-  </div>
-);
+const Collection = ({ collection }) => {
+  const { title, items } = collection;
+  const itemsList = items.map(item => (
+    <CollectionItem key={item.id} item={item} />
+  ));
+  return (
+    <div className="collection">
+      <h2 className="title">{title}</h2>
+      <div className="items">{itemsList}</div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
