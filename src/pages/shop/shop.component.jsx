0a1b33f7394d39069ae2f0f3335386c81bc1866e
Moosees <linus.almgren@gmail.com>
2019-08-03 00:05:10 +0200

    Add action to update shop reducer with collections data from firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import Collection from '../collection/collection.component';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils.js';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions.js';
 
-const Shop = ({ match }) => (
-  <>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={Collection} />
-  </>
-);
+class Shop extends Component {
+  unsubscribeFromSnapshot = null;
 
-export default Shop;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionsRef = firestore.collection('collections');
+    collectionsRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
+  }
+
+  render() {
+    const match = this.props;
+    return (
+      <>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Shop);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,9 +1,16 @@
+import { ShopActionTypes } from './shop.types';
 import { collections } from './shop.initial-state';
 
 const INITIAL_STATE = { collections };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload
+      };
+
     default:
       return state;
   }
